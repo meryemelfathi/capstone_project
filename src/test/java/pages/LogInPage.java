d5538e9d155e7f0ef63f394fc6c8a6294132d0a2
Mohammed <meryemyoumos@gmail.com>
2022-06-27 18:04:28 -0600

    fff

--- src/test/java/pages/LogInPage.java
+++ src/test/java/pages/LogInPage.java
@@ -2,6 +2,10 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LogInPage {
 
@@ -13,12 +17,15 @@ public class LogInPage {
     //Constructor that will be automatically called as soon as the object of the class is created
     public LogInPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
 
     // here we identify elements which we want to use tests
 
     //Locator for Email field
+    @FindBy(linkText = "My account")
+    WebElement myAccountLink;
     By yourEmail = By.id("ispbxii_1");
 
     //Locator for password field
@@ -26,6 +33,9 @@ public class LogInPage {
 
     //
     By login = By.xpath("//*[@id=\"customer_login\"]/div[3]/input");
+    @FindBy(xpath="//div[@class='customer-links-inner']/ul/li[3]/a")
+
+    WebElement logOutLink;
 
 
     //Here we create actions which we want to do over elements
@@ -33,5 +43,16 @@ public class LogInPage {
     public void enterYourEmail(String yEmail) { driver.findElement(yourEmail).sendKeys(yEmail);}
     public void enterYourPassword(String yPasw) { driver.findElement(yourPassword).sendKeys(yPasw);}
     public void clickLogin() { driver.findElement(login).click();}
+    public void hooverMyAccountLink() {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(myAccountLink).perform();
+
+    }
+    public void MyAccountLink() {
+
+        myAccountLink.click();
+
+    }
+    public void clickLogOutLink() {logOutLink.click();}
 
 }
--- src/test/java/testcases/RegistrationTests.java
+++ src/test/java/testcases/RegistrationTests.java
@@ -10,6 +10,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.AccountPage;
 import pages.HomePage;
+import pages.LogInPage;
 import pages.SignInPage;
 
 import java.io.IOException;
@@ -25,6 +26,7 @@ public class RegistrationTests {
     HomePage homePage;
     AccountPage accountPage;
     SignInPage signInPage;
+    LogInPage logInPage;
     @BeforeSuite
     public void launchReport() {
         setUpReport("RegistrationTests.html");
@@ -47,39 +49,74 @@ public class RegistrationTests {
 
     //test1 : new user registration page
     @Test (priority = 1)
-    public void registrationPage()
-    {
+    public void registrationPage() throws IOException {
+        Reports.test = Reports.extent.createTest("registrationPage", "Test Pass");
+
         homePage = new HomePage(driver);
+        Reports.takeScreenShot(driver, System.getProperty("user.dir") + "/test-output/TC1-img1.jpg");
+        Reports.test.addScreenCaptureFromPath("TC1-img1.jpg");
         homePage.clickAccountLink();
         accountPage = new AccountPage(driver);
         accountPage.register();
-
+        Reports.takeScreenShot(driver, System.getProperty("user.dir") + "/test-output/TC1-img2.jpg");
+        Reports.test.addScreenCaptureFromPath("TC1-img2.jpg");
         String expected = homePage.assertSignIn();
         String actual = "Sign up";
+        Reports.takeScreenShot(driver, System.getProperty("user.dir") + "/test-output/TC1-img3.jpg");
+        Reports.test.addScreenCaptureFromPath("TC1-img3.jpg");
         Assert.assertEquals(actual, expected);
     }
 
 
     //test2 : verify register new user
-    @Test
-    public void registration() {
+    @Test (priority = 2)
+    public void registration() throws InterruptedException, IOException {
+        Reports.test = Reports.extent.createTest("registration", "Test Pass");
 
+        homePage = new HomePage(driver);
+        homePage.clickAccountLink();
+        Reports.takeScreenShot(driver, System.getProperty("user.dir") + "/test-output/TC2-img1.jpg");
+        Reports.test.addScreenCaptureFromPath("TC2-img1.jpg");
+        accountPage = new AccountPage(driver);
+        accountPage.register();
         //test = reports.createTest("registration", "Test Passed");
         signInPage = new SignInPage(driver);
 
         signInPage.enterFirstName("meryem");
         signInPage.enterLastName("el fathi");
-        signInPage.enterEmail("meryemElFathi1@gmail.com");
+        signInPage.enterEmail("meryemElFathi020@gmail.com");
         signInPage.enterPassword("abc123");
-
+        Reports.takeScreenShot(driver, System.getProperty("user.dir") + "/test-output/TC2-img2.jpg");
+        Reports.test.addScreenCaptureFromPath("TC2-img2.jpg");
         signInPage.clickRegister();
+        logInPage=new LogInPage(driver);
+
+        Thread.sleep(30000);
+        logInPage=new LogInPage(driver);
+      logInPage.MyAccountLink();
+      logInPage.hooverMyAccountLink();
+        Reports.takeScreenShot(driver, System.getProperty("user.dir") + "/test-output/TC2-img3.jpg");
+        Reports.test.addScreenCaptureFromPath("TC2-img3.jpg");
+        logInPage.clickLogOutLink();
+        driver.get("https://www.alexandnova.com");
+
 
     }
 
     //test3 : email validation
-    @Test
-    public void emailValidation() {
+    @Test (priority = 3)
+    public void emailValidation() throws InterruptedException, IOException {
+        Reports.test = Reports.extent.createTest("emailValidation", "Test Pass");
 
+        homePage = new HomePage(driver);
+        homePage.clickAccountLink();
+        Reports.takeScreenShot(driver, System.getProperty("user.dir") + "/test-output/TC3-img1.jpg");
+        Reports.test.addScreenCaptureFromPath("TC3-img1.jpg");
+
+        accountPage = new AccountPage(driver);
+        accountPage.register();
+        Reports.takeScreenShot(driver, System.getProperty("user.dir") + "/test-output/TC3-img2.jpg");
+        Reports.test.addScreenCaptureFromPath("TC3-img2.jpg");
 
         signInPage = new SignInPage(driver);
 
@@ -87,12 +124,14 @@ public class RegistrationTests {
         signInPage.enterLastName("fathi");
         signInPage.enterEmail("meryemElFathi.gmail");
         signInPage.enterPassword("123abc");
-
+        Reports.takeScreenShot(driver, System.getProperty("user.dir") + "/test-output/TC3-img3.jpg");
+        Reports.test.addScreenCaptureFromPath("TC3-img3.jpg");
         signInPage.clickRegister();
+        Thread.sleep(30000);
     }
 
     //test4 : Required / mandatory fields
-    @Test
+    @Test (priority = 4 ,enabled = false)
     public void mandatoryFields() {
 
 
@@ -102,18 +141,27 @@ public class RegistrationTests {
 
     //test5 : Verify if the password required rules are not satisfied in the password
 
-    @Test
-    public void passwordValidation() {
+    @Test (priority = 5)
+    public void passwordValidation() throws InterruptedException, IOException {
+        Reports.test = Reports.extent.createTest("passwordValidation", "Test Pass");
 
-
+        homePage = new HomePage(driver);
+        homePage.clickAccountLink();
+        accountPage=new AccountPage(driver);
+        accountPage.register();
+        Reports.takeScreenShot(driver, System.getProperty("user.dir") + "/test-output/TC5-img1.jpg");
+        Reports.test.addScreenCaptureFromPath("TC5-img1.jpg");
         signInPage = new SignInPage(driver);
 
         signInPage.enterFirstName("meryem");
         signInPage.enterLastName("fathi");
         signInPage.enterEmail("meryem@gmail.com");
         signInPage.enterPassword("passw");
+        Reports.takeScreenShot(driver, System.getProperty("user.dir") + "/test-output/TC5-img2.jpg");
+        Reports.test.addScreenCaptureFromPath("TC5-img2.jpg");
 
         signInPage.clickRegister();
+        Thread.sleep(30000);
     }
 
     @AfterTest
